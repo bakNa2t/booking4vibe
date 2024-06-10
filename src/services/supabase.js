215f9e4bf1885dfd93d008a604e92f628198c106
bakNa2t <ok2.lock@gmail.com>
2024-06-10 15:37:23 +0300

    Added supabase client

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://celamgsaxrexxdabtnlt.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlbGFtZ3NheHJleHhkYWJ0bmx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwMDkzMzMsImV4cCI6MjAzMzU4NTMzM30.7FXFUiqgCpYOzU5MNT1PwQsk4SwslvMIyWmXu4dTf5Y";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
