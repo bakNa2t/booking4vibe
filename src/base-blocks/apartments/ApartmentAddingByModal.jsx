72ff365e32a8be7fb08f16afa530019974f34dd3
bakNa2t <ok2.lock@gmail.com>
2024-06-15 12:53:16 +0300

    Added ApartmentAddingByModal to Apartments

---
+++ src/base-blocks/apartments/ApartmentAddingByModal.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+import Button from "../../ui-blocks/Button";
+import Modal from "../../ui-blocks/Modal";
+// import ApartmentsCreatingForm from "./ApartmentsCreatingForm";
+
+function ApartmentAddingByModal() {
+  const [isOpneModal, setIsOpneModal] = useState(false);
+
+  return (
+    <div>
+      <Button onClick={() => setIsOpneModal(!isOpneModal)}>
+        Add new apartment
+      </Button>
+      {/* {isOpneModal && <ApartmentsCreatingForm />} */}
+      {isOpneModal && <Modal />}
+    </div>
+  );
+}
+
+export default ApartmentAddingByModal;
