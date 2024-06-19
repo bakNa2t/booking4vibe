e0eb79fa749ed1a3c89edb0a9d37c552391c2898
bakNa2t <ok2.lock@gmail.com>
2024-06-19 11:32:59 +0300

    Added searchParams to create filteredApartmnets to filter for three if statements

--- src/base-blocks/apartments/ApartmentsTable.jsx
+++ src/base-blocks/apartments/ApartmentsTable.jsx
@@ -6,34 +6,29 @@ import Table from "../../ui-blocks/Table";
 
 import { useApartments } from "./useApartments";
 import MenuRow from "../../ui-blocks/MenuRow";
-
-// const Table = styled.div`
-//   border: 1px solid var(--color-emerald-200);
-
-//   font-size: 1.4rem;
-//   background-color: var(--color-grey-0);
-//   border-radius: 7px;
-//   overflow: hidden;
-// `;
-
-// const TableHeader = styled.header`
-//   display: grid;
-//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-//   column-gap: 2.4rem;
-//   align-items: center;
-
-//   background-color: var(--color-grey-50);
-//   border-bottom: 1px solid var(--color-grey-100);
-//   text-transform: uppercase;
-//   letter-spacing: 0.4px;
-//   font-weight: 600;
-//   color: var(--color-grey-600);
-//   padding: 1.6rem 2.4rem;
-// `;
+import { useSearchParams } from "react-router-dom";
 
 function ApartmentsTable() {
   const { isLoading, apartments } = useApartments();
+  const [searchParams] = useSearchParams();
+
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredApartments;
+
+  if (filterValue === "all") filteredApartments = apartments;
+
+  if (filterValue === "no-discount")
+    filteredApartments = apartments.filter(
+      (apartment) => apartment.discount === 0
+    );
+
+  if (filterValue === "with-discount")
+    filteredApartments = apartments.filter(
+      (apartment) => apartment.discount > 0
+    );
 
   return (
     <MenuRow>
@@ -48,7 +43,7 @@ function ApartmentsTable() {
         </Table.Header>
 
         <Table.Body
-          data={apartments}
+          data={filteredApartments}
           render={(apartment) => (
             <ApartmentsRow key={apartment.id} apartment={apartment} />
           )}
