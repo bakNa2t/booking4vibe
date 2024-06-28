472cb930f421169319e9905b42e0cd8577b9f6bf
bakNa2t <ok2.lock@gmail.com>
2024-06-28 14:48:20 +0300

    Updated filter's handleClick fn added searchParams validation for last page

--- src/ui-blocks/Filter.jsx
+++ src/ui-blocks/Filter.jsx
@@ -46,6 +46,8 @@ function Filter({ filterField, options }) {
 
   function handleClick(value) {
     searchParams.set(filterField, value);
+    if (searchParams.get("page")) searchParams.set("page", 1);
+
     setSearchParams(searchParams);
   }
 
