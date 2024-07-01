7dc6a502ab77305a1b549a39a03b07ea76345480
bakNa2t <ok2.lock@gmail.com>
2024-07-01 17:10:04 +0300

    Added queryClient to get authenticated user from cash

--- src/base-blocks/authentication/useLogIn.js
+++ src/base-blocks/authentication/useLogIn.js
@@ -1,14 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
-import { login as loginApi } from "../../services/apiAuth";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
+import { login as loginApi } from "../../services/apiAuth";
+
 export function useLogIn() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user);
       navigate("/dashboard");
     },
     onError: (error) => {
