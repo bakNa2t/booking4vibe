7fd0bb08d6b5c5747de40ddaf489a2d6c4a90be8
bakNa2t <ok2.lock@gmail.com>
2024-07-01 16:41:10 +0300

    Added useGetUser to ProtectedRoute

--- src/ui-blocks/ProtectedRoute.jsx
+++ src/ui-blocks/ProtectedRoute.jsx
@@ -1,4 +1,39 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import Spinner from "./Spinner";
+
+import { useGetUser } from "../base-blocks/authentication/useGetUser";
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--color-emerald-50);
+`;
+
 function ProtectedRoute({ children }) {
+  ProtectedRoute.propTypes = {
+    children: PropTypes.node,
+  };
+
+  // --> load the authenticated user
+  const { user, isLoading } = useGetUser();
+
+  // --> Dispaly spinner while loading
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  // --> if not authenticated, redirect to login page
+
+  // --> if authenticated, render app's children
+  console.log(user);
+
   return children;
 }
 
