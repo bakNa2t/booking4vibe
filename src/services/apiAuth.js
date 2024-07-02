fd7c1ace7451730a4ac9a05f3763832b25c7c5f5
bakNa2t <ok2.lock@gmail.com>
2024-07-02 15:45:29 +0300

    Updated apiAuth to add signup fn

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,21 @@
 import supabase from "./supabase";
 
+// Sign up with email and password
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+  return data;
+}
 // Log in with email and password
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
