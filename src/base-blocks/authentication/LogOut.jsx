d6afe607ab50da21d822ee848c4606f5f19e50d8
bakNa2t <ok2.lock@gmail.com>
2024-07-03 14:37:09 +0300

    Added Title and HeaderMenuWrapper to Header

--- src/base-blocks/authentication/LogOut.jsx
+++ src/base-blocks/authentication/LogOut.jsx
@@ -1,16 +1,9 @@
-// import styled from "styled-components";
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
 
 import ButtonIcon from "../../ui-blocks/ButtonIcon";
 import SpinnerSmall from "../../ui-blocks/SpinnerSmall";
 
 import { useLogOut } from "./useLogOut";
-
-// const Wrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-//   gap: 0.8rem;
-// `;
 
 function LogOut() {
   const { logout, isLoading } = useLogOut();
--- src/ui-blocks/Header.jsx
+++ src/ui-blocks/Header.jsx
@@ -4,19 +4,36 @@ import HeaderMenu from "./HeaderMenu";
 import AvatarUser from "../base-blocks/authentication/AvatarUser";
 
 const StyledHeader = styled.header`
-  /* display: flex;
+  display: flex;
   align-items: center;
-  justify-content: space-between; */
+  justify-content: space-between;
+
   background-color: var(--color-emerald-0);
   padding: 2rem 4rem;
   border-bottom: 1px solid var(--color-emerald-200);
 `;
 
+const Title = styled.div`
+  font-size: 2.4rem;
+  font-weight: 600;
+  color: var(--color-emerald-600);
+`;
+
+const HeaderMenuWrapper = styled.div`
+  display: flex;
+  gap: 3.4rem;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
 function Header() {
   return (
     <StyledHeader>
-      <AvatarUser />
-      <HeaderMenu />
+      <Title>Booking4vibe</Title>
+      <HeaderMenuWrapper>
+        <AvatarUser />
+        <HeaderMenu />
+      </HeaderMenuWrapper>
     </StyledHeader>
   );
 }
