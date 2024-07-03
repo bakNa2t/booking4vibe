f1255caf4b48ea4f412b48e393dd0532402b9022
bakNa2t <ok2.lock@gmail.com>
2024-07-03 17:57:23 +0300

    Updated apiAuth by adding updateCurrentUser fn

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 // Sign up with email and password
 export async function signup({ fullName, email, password }) {
@@ -43,4 +43,40 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+// Update user's account data
+export async function updateCurrentUser({ fullName, password, avatar }) {
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  // Update fullName or password
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+
+  if (!avatar) return data;
+
+  // Upload avatar
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // Update user's avatar
+
+  const { data: updateUser, error: updateUserError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+
+  if (updateUserError) throw new Error(updateUserError.message);
+
+  return updateUser;
 }
