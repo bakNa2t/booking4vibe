9122d462070b2a7b3392c0ed89ae3942540fe897
bakNa2t <ok2.lock@gmail.com>
2024-07-05 14:24:09 +0300

    Added dark-mode btn to Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
+import { HiOutlineMoon } from "react-icons/hi";
 import styled from "styled-components";
 
 import FormLogin from "../base-blocks/authentication/FormLogin";
 import Logo from "../ui-blocks/Logo";
 import Heading from "../ui-blocks/Heading";
+import ButtonIcon from "../ui-blocks/ButtonIcon";
 
 const LoginLayout = styled.main`
   min-height: 100vh;
@@ -14,9 +16,21 @@ const LoginLayout = styled.main`
   background-color: var(--color-emerald-50);
 `;
 
+const DarkModeToggle = styled.div`
+  position: fixed;
+  top: 1rem;
+  right: 1rem;
+  z-index: 1000;
+`;
+
 function Login() {
   return (
     <LoginLayout>
+      <DarkModeToggle>
+        <ButtonIcon>
+          <HiOutlineMoon />
+        </ButtonIcon>
+      </DarkModeToggle>
       <Logo />
       <Heading as="h4">Log in to your account</Heading>
       <FormLogin />
