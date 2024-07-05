d971c06ccb4bca87b2be4c874b108dae95c174c3
bakNa2t <ok2.lock@gmail.com>
2024-07-05 11:25:59 +0300

    Updated app colors for dark-mode theme

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,7 +2,80 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
     :root {
-  /* Indigo */
+  &, &.light-mode {
+      /* secondary colors */
+    --color-emerald-0: #f9fefc;
+    --color-emerald-50: #ecfdf5;
+    --color-emerald-100: #d1fae5;
+    --color-emerald-200: #a7f3d0;
+    --color-emerald-300: #6ee7b7;
+    --color-emerald-400: #34d399;
+    --color-emerald-500: #10b981;
+    --color-emerald-600: #059669;
+    --color-emerald-700: #047857;
+    --color-emerald-800: #065f46;
+    --color-emerald-900: #064e3b;
+
+    --color-blue-100: #e0f2fe;
+    --color-blue-700: #0369a1;
+    --color-green-100: #dcfce7;
+    --color-green-700: #15803d;
+    --color-yellow-100: #fef9c3;
+    --color-yellow-700: #a16207;
+    --color-silver-100: #e5e7eb;
+    --color-silver-700: #374151;
+    --color-indigo-100: #e0e7ff;
+    --color-indigo-700: #4338ca;
+
+    --backdrop-color: rgba(255, 255, 255, 0.1);
+
+    --shadow-sm: 0 1px 2px rgba(0, 0, 0, 0.04);
+    --shadow-md: 0px 0.6rem 2.4rem rgba(0, 0, 0, 0.06);
+    --shadow-lg: 0 2.4rem 3.2rem rgba(0, 0, 0, 0.12);
+    
+    --image-grayscale: 0;
+    --image-opacity: 100%;
+  }
+
+  &.dark-mode{
+    --color-emerald-0: #064e3b;
+    --color-emerald-50: #065f46;
+    --color-emerald-100: #047857;
+    --color-emerald-200: #059669;
+    --color-emerald-300: #10b981;
+    --color-emerald-400: #34d399;
+    --color-emerald-500: #6ee7b7;
+    --color-emerald-600: #a7f3d0;
+    --color-emerald-700: #d1fae5;
+    --color-emerald-800: #ecfdf5;
+    --color-emerald-900: #f9fefc;
+    
+    --color-blue-100: #075985;
+    --color-blue-700: #e0f2fe;
+    --color-green-100: #166534;
+    --color-green-700: #dcfce7;
+    --color-yellow-100: #854d0e;
+    --color-yellow-700: #fef9c3;
+    --color-silver-100: #374151;
+    --color-silver-700: #f3f4f6;
+    --color-indigo-100: #3730a3;
+    --color-indigo-700: #e0e7ff;
+    
+    /* --color-red-100: #fee2e2;
+    --color-red-700: #b91c1c;
+    --color-red-800: #991b1b; */
+    
+    --backdrop-color: rgba(0, 0, 0, 0.3);
+    
+    --shadow-sm: 0 1px 2px rgba(0, 0, 0, 0.4);
+    --shadow-md: 0px 0.6rem 2.4rem rgba(0, 0, 0, 0.3);
+    --shadow-lg: 0 2.4rem 3.2rem rgba(0, 0, 0, 0.4);
+    
+    --image-grayscale: 10%;
+    --image-opacity: 90%;
+  }
+
+  /* base colors */
   --color-brand-50:#fff8e2;
   --color-brand-100: #fff1c0;
   --color-brand-200: #ffe888;
@@ -14,48 +87,14 @@ const GlobalStyles = createGlobalStyle`
   --color-brand-800: #737000;
   --color-brand-900: #616106;
 
-  /* Emerald */
-  --color-emerald-0: #fff;
-  --color-emerald-50: #ecfdf5;
-  --color-emerald-100: #d1fae5;
-  --color-emerald-200: #a7f3d0;
-  --color-emerald-300: #6ee7b7;
-  --color-emerald-400: #34d399;
-  --color-emerald-500: #10b981;
-  --color-emerald-600: #059669;
-  --color-emerald-700: #047857;
-  --color-emerald-800: #065f46;
-  --color-emerald-900: #064e3b;
-
-  --color-blue-100: #e0f2fe;
-  --color-blue-700: #0369a1;
-  --color-green-100: #dcfce7;
-  --color-green-700: #15803d;
-  --color-yellow-100: #fef9c3;
-  --color-yellow-700: #a16207;
-  --color-silver-100: #e5e7eb;
-  --color-silver-700: #374151;
-  --color-indigo-100: #e0e7ff;
-  --color-indigo-700: #4338ca;
-
   --color-red-100: #fee2e2;
   --color-red-700: #b91c1c;
   --color-red-800: #991b1b;
-
-  --backdrop-color: rgba(255, 255, 255, 0.1);
-
-  --shadow-sm: 0 1px 2px rgba(0, 0, 0, 0.04);
-  --shadow-md: 0px 0.6rem 2.4rem rgba(0, 0, 0, 0.06);
-  --shadow-lg: 0 2.4rem 3.2rem rgba(0, 0, 0, 0.12);
 
   --border-radius-tiny: 3px;
   --border-radius-sm: 5px;
   --border-radius-md: 7px;
   --border-radius-lg: 9px;
-
-  /* For dark mode */
-  --image-grayscale: 0;
-  --image-opacity: 100%;
 }
 
 *,
@@ -148,17 +187,17 @@ img {
 /*
   FOR DARK MODE
   
-  --color-grey-0: #18212f;
-  --color-grey-50: #111827;
-  --color-grey-100: #1f2937;
-  --color-grey-200: #374151;
-  --color-grey-300: #4b5563;
-  --color-grey-400: #6b7280;
-  --color-grey-500: #9ca3af;
-  --color-grey-600: #d1d5db;
-  --color-grey-700: #e5e7eb;
-  --color-grey-800: #f3f4f6;
-  --color-grey-900: #f9fafb;
+  --color-emerald-0: #064e3b;
+  --color-emerald-50: #065f46;
+  --color-emerald-100: #047857;
+  --color-emerald-200: #059669;
+  --color-emerald-300: #10b981;
+  --color-emerald-400: #34d399;
+  --color-emerald-500: #6ee7b7;
+  --color-emerald-600: #a7f3d0;
+  --color-emerald-700: #d1fae5;
+  --color-emerald-800: #ecfdf5;
+  --color-emerald-900: #f9fefc;
   
   --color-blue-100: #075985;
   --color-blue-700: #e0f2fe;
