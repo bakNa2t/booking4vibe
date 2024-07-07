c23d5526a4789237d9238ac03d7b3061a780bd07
bakNa2t <ok2.lock@gmail.com>
2024-07-07 12:54:03 +0300

    Added DarModeToggle to Signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import FormSignup from "../base-blocks/authentication/FormSignup";
 import Heading from "../ui-blocks/Heading";
 import Logo from "../ui-blocks/Logo";
+import DarkModeToggle from "../ui-blocks/DarkModeToggle";
+import DarkModeWrapper from "../ui-blocks/DarkModeWrapper";
 
 const SignupLayout = styled.main`
   min-height: 100vh;
@@ -16,6 +18,9 @@ const SignupLayout = styled.main`
 function Signip() {
   return (
     <SignupLayout>
+      <DarkModeWrapper>
+        <DarkModeToggle />
+      </DarkModeWrapper>
       <Logo />
       <Heading as="h4">Sign up for an account</Heading>
       <FormSignup />
