0d7a72a8b96d65f7cabc839011fdd56c21b6911a
bakNa2t <ok2.lock@gmail.com>
2024-07-08 15:50:31 +0300

    Created useRecentBookings

---
+++ src/base-blocks/dashboard/useRecentBookings.js
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+import { getBookingsAfterDate } from "../../services/apiForBooking";
+
+export function useRecentBookimgs() {
+  const [searchParams] = useSearchParams();
+
+  const amountDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+
+  const queryDate = subDays(new Date(), amountDays).toISOString();
+
+  const { isLoading, data: bookings } = useQuery({
+    queryFn: () => getBookingsAfterDate(queryDate),
+    queryKey: ["bookings", `last - ${amountDays}`],
+  });
+
+  return { isLoading, bookings };
+}
--- src/services/apiForBooking.js
+++ src/services/apiForBooking.js
@@ -1,4 +1,4 @@
-// import { getToday } from "../utils/utilsFunctions";
+import { getToday } from "../utils/utilsFunctions";
 import supabase from "./supabase";
 import { PAGE_SIZE } from "../utils/constants";
 
@@ -51,8 +51,9 @@ export async function getBooking(id) {
   return data;
 }
 
-// Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
-/*export async function getBookingsAfterDate(date) {
+// Returns all BOOKINGS that are were created after the given date.
+//Useful to get bookings created in the last 30 days, for example.
+export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
     .select("created_at, totalPrice, extrasPrice")
@@ -65,16 +66,15 @@ export async function getBooking(id) {
   }
 
   return data;
-}*/
+}
 
 // Returns all STAYS that are were created after the given date
-/*export async function getStaysAfterDate(date) {
+export async function getStaysAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
-    // .select('*')
     .select("*, guests(fullName)")
-    .gte("startDate", date)
-    .lte("startDate", getToday());
+    .gte("startDay", date)
+    .lte("startDay", getToday());
 
   if (error) {
     console.error(error);
@@ -82,7 +82,7 @@ export async function getBooking(id) {
   }
 
   return data;
-}*/
+}
 
 // Activity means that there is a check in or a check out today
 /*export async function getStaysTodayActivity() {
