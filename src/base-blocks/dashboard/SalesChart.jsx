4ea83d403c4d32b8a20e95bba0208f164b51e5ad
bakNa2t <ok2.lock@gmail.com>
2024-07-09 15:32:04 +0300

    Updated props style of AraeChart

--- src/base-blocks/dashboard/SalesChart.jsx
+++ src/base-blocks/dashboard/SalesChart.jsx
@@ -12,6 +12,8 @@ import {
 import DashboardBox from "./DashboardBox";
 import Heading from "../../ui-blocks/Heading";
 
+import { useDarkMode } from "../../context/DarkModeContetx";
+
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
 
@@ -54,23 +56,22 @@ const fakeData = [
   { label: "Feb 06", totalSales: 1450, extrasSales: 400 },
 ];
 
-const isDarkMode = true;
-const colors = isDarkMode
-  ? {
-      totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
-      extrasSales: { stroke: "#22c55e", fill: "#22c55e" },
-      text: "#e5e7eb",
-      background: "#18212f",
-    }
-  : {
-      totalSales: { stroke: "#4f46e5", fill: "#c7d2fe" },
-      extrasSales: { stroke: "#16a34a", fill: "#dcfce7" },
-      text: "#374151",
-      background: "#fff",
-    };
-
 function SalesChart() {
-  console.log(colors);
+  const { isDarkMode } = useDarkMode();
+
+  const colors = isDarkMode
+    ? {
+        totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
+        extrasSales: { stroke: "#22c55e", fill: "#22c55e" },
+        text: "#e5e7eb",
+        background: "#18212f",
+      }
+    : {
+        totalSales: { stroke: "#4f46e5", fill: "#c7d2fe" },
+        extrasSales: { stroke: "#16a34a", fill: "#dcfce7" },
+        text: "#374151",
+        background: "#fff",
+      };
 
   return (
     <StyledSalesChart>
@@ -79,14 +80,34 @@ function SalesChart() {
       <ResponsiveContainer width={"100%"} height={300}>
         <AreaChart data={fakeData}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey={"label"} />
-          <YAxis unit={"$"} />
-          <Tooltip />
+          <XAxis
+            dataKey={"label"}
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
+          <YAxis
+            unit={"$"}
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
+          <Tooltip contentStyle={{ backgroundColor: colors.background }} />
           <Area
             dataKey={"totalSales"}
             type={"monotone"}
-            stroke="#4f46e5"
-            fill="#9f40f4"
+            stroke={colors.totalSales.stroke}
+            fill={colors.totalSales.fill}
+            strokeWidth={2}
+            name="Total Sales"
+            unit={"$"}
+          />
+          <Area
+            dataKey={"extrasSales"}
+            type={"monotone"}
+            stroke={colors.extrasSales.stroke}
+            fill={colors.extrasSales.fill}
+            strokeWidth={2}
+            name="Extras Sales"
+            unit={"$"}
           />
         </AreaChart>
       </ResponsiveContainer>
