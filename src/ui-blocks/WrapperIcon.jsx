19e25c3b9eae9dd15c0c1fed922e325a00ae183f
bakNa2t <ok2.lock@gmail.com>
2024-07-20 10:40:01 +0300

    Created WrapperIcon

---
+++ src/ui-blocks/WrapperIcon.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const WrapperIcon = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 1rem;
+  transform: translateY(-50%);
+  cursor: pointer;
+  font-size: 1.6rem;
+`;
+
+export default WrapperIcon;
